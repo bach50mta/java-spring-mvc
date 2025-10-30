fce7d8ae8eb8957384cbc82c9d01216582c124c7
bach50mta <bach50mta@gmail.com>
2025-10-30 16:53:17 +0700

    update befor video 51

--- src/main/java/com/devb/laptopshop/controller/UserController.java
+++ src/main/java/com/devb/laptopshop/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.devb.laptopshop.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-
 import com.devb.laptopshop.service.UserService;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class UserController {
 
     private UserService userService;
@@ -15,8 +18,25 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "hello";
     }
+
 }
+
+// @RestController
+// public class UserController {
+
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("/")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
